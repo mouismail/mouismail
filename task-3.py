1b6f182d229eb6f6b0e3e769e0d70c2354145c3d
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:47:54 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -4,29 +4,38 @@ class Tree:
         self.l = left
         self.r = right
 
-def reroot(tree, parent):
-    if tree is None:
-        return parent
-    
-    left_subtree = reroot(tree.l, Tree(tree.x, None, parent))
-    right_subtree = reroot(tree.r, left_subtree)
-    
-    tree.l = None
-    tree.r = None
-    
-    return right_subtree
-
 def solution(T, leaf_id):
-    # Helper function to find the leaf node to reroot the tree
-    def find_leaf_and_reroot(current, parent):
+    # Convert tuple to Tree
+    def tuple_to_tree(t):
+        if not t:
+            return None
+        return Tree(t[0], tuple_to_tree(t[1]), tuple_to_tree(t[2]))
+
+    # Find and reroot the tree
+    def find_and_reroot(current, parent):
         if current is None:
             return None
         if current.x == leaf_id:
             return reroot(current, parent)
-        left_result = find_leaf_and_reroot(current.l, Tree(current.x, None, parent))
+        # Recurse on left and right subtrees
+        left_result = find_and_reroot(current.l, Tree(current.x, None, parent))
         if left_result is not None:
             return left_result
-        right_result = find_leaf_and_reroot(current.r, Tree(current.x, parent, None))
+        right_result = find_and_reroot(current.r, Tree(current.x, parent, None))
         return right_result
-    
-    return find_leaf_and_reroot(T, None)
+
+    # Perform the rerooting
+    def reroot(node, parent):
+        if node is None:
+            return parent
+        # The order of the next two lines is changed to first process the right subtree
+        right_subtree = reroot(node.r, Tree(node.x, None, parent))
+        left_subtree = reroot(node.l, right_subtree)
+        node.l = None
+        node.r = None
+        return left_subtree
+
+    # Convert the input tuple to a Tree
+    root = tuple_to_tree(T)
+    # Find the leaf and reroot the tree
+    return find_and_reroot(root, None)
