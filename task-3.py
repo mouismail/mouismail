b5924539dc1016cb736a2e06b63e2b1c9f29ce4b
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:02:26 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -1,43 +1,38 @@
-class Tree:
-  """
-  This class represents a binary tree node.
-  """
-  def __init__(self, x, l=None, r=None):
-    self.x = x
-    self.l = l
-    self.r = r
-
 def solution(tree, leaf_id):
-  """
-  This function reroots the tree at the specified leaf node.
-  """
-  # Helper function to recursively find the leaf node and its parents
-  def find_leaf_and_parents(node, parent=None):
-    if node is None:
-      return None, []
-    if node.x == leaf_id:
-      return node, [parent] if parent else []
-    # Recur for the left and right subtrees
-    left_leaf, left_parents = find_leaf_and_parents(node.l, node)
-    if left_leaf:
-      return left_leaf, [parent] + left_parents if parent else left_parents
-    right_leaf, right_parents = find_leaf_and_parents(node.r, node)
-    if right_leaf:
-      return right_leaf, [parent] + right_parents if parent else right_parents
-    return None, []
+    """
+    This function reroots the tree at the specified leaf node.
+    """
+    # Helper function to recursively find the path from root to the leaf
+    def find_path(node, path):
+        if node is None:
+            return False
+        if node.x == leaf_id:
+            path.append(node)
+            return True
+        path.append(node)
+        if find_path(node.l, path) or find_path(node.r, path):
+            return True
+        path.pop()
+        return False
 
-  # Find the leaf node and its parents
-  leaf_node, parents = find_leaf_and_parents(tree)
-  # Reroot the tree by reversing the parent-child relationship
-  for i in range(len(parents) - 1):
-    if parents[i].l is parents[i + 1]:
-      parents[i].l = parents[i - 1] if i > 0 else None
-    else:
-      parents[i].r = parents[i - 1] if i > 0 else None
-  if parents and parents[-1].l is leaf_node:
-    parents[-1].l = parents[-2] if len(parents) > 1 else None
-  elif parents:
-    parents[-1].r = parents[-2] if len(parents) > 1 else None
-  leaf_node.l = leaf_node.r = None
-  leaf_node.l = parents[0] if parents else None
-  return leaf_node
+    path = []
+    find_path(tree, path)
+
+    for i in range(len(path) - 1, 0, -1):
+        current_node = path[i]
+        parent_node = path[i - 1]
+        # Swap children and parent
+        if parent_node.l == current_node:
+            parent_node.l = None
+        else:
+            parent_node.r = None
+
+        if current_node.l is None:
+            current_node.l = parent_node
+        else:
+            current_node.r = parent_node
+
+    # The last node in the path is the new root
+    if path:
+        path[-1].l = path[-1].r = None
+        return path[-1]
