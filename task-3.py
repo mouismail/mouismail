02982ba3a16f71b13ea6ae644692df653148ddd2
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 20:50:22 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -1,37 +1,43 @@
 class Tree:
-    def __init__(self, x, l=None, r=None):
-        self.x = x
-        self.l = l
-        self.r = r
+  """
+  This class represents a binary tree node.
+  """
+  def __init__(self, x, l=None, r=None):
+    self.x = x
+    self.l = l
+    self.r = r
 
-def solution(T, leaf_id):
-    # Helper function to recursively find the leaf node and its parents
-    def find_leaf_and_parents(node, parent=None):
-        if node is None:
-            return None, []
-        if node.x == leaf_id:
-            return node, [parent]
-        # Recur for the left and right subtrees
-        left_leaf, left_parents = find_leaf_and_parents(node.l, node)
-        if left_leaf:
-            return left_leaf, [parent] + left_parents
-        right_leaf, right_parents = find_leaf_and_parents(node.r, node)
-        if right_leaf:
-            return right_leaf, [parent] + right_parents
-        return None, []
+def solution(tree, leaf_id):
+  """
+  This function reroots the tree at the specified leaf node.
+  """
+  # Helper function to recursively find the leaf node and its parents
+  def find_leaf_and_parents(node, parent=None):
+    if node is None:
+      return None, []
+    if node.x == leaf_id:
+      return node, [parent] if parent else []
+    # Recur for the left and right subtrees
+    left_leaf, left_parents = find_leaf_and_parents(node.l, node)
+    if left_leaf:
+      return left_leaf, [parent] + left_parents if parent else left_parents
+    right_leaf, right_parents = find_leaf_and_parents(node.r, node)
+    if right_leaf:
+      return right_leaf, [parent] + right_parents if parent else right_parents
+    return None, []
 
-    # Find the leaf node and its parents
-    leaf_node, parents = find_leaf_and_parents(T)
-    # Reroot the tree by reversing the parent-child relationship
-    for i in range(len(parents) - 1):
-        if parents[i].l is parents[i + 1]:
-            parents[i].l = parents[i - 1] if i > 0 else None
-        else:
-            parents[i].r = parents[i - 1] if i > 0 else None
-    if parents[-1].l is leaf_node:
-        parents[-1].l = parents[-2] if len(parents) > 1 else None
+  # Find the leaf node and its parents
+  leaf_node, parents = find_leaf_and_parents(tree)
+  # Reroot the tree by reversing the parent-child relationship
+  for i in range(len(parents) - 1):
+    if parents[i].l is parents[i + 1]:
+      parents[i].l = parents[i - 1] if i > 0 else None
     else:
-        parents[-1].r = parents[-2] if len(parents) > 1 else None
-    leaf_node.l = leaf_node.r = None
-    leaf_node.l = parents[0] if parents else None
-    return leaf_node
+      parents[i].r = parents[i - 1] if i > 0 else None
+  if parents and parents[-1].l is leaf_node:
+    parents[-1].l = parents[-2] if len(parents) > 1 else None
+  elif parents:
+    parents[-1].r = parents[-2] if len(parents) > 1 else None
+  leaf_node.l = leaf_node.r = None
+  leaf_node.l = parents[0] if parents else None
+  return leaf_node
