f000f265044dc80b5ea24352ba95e7b99f6ef809
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:39:20 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -11,40 +11,35 @@ def solution(tree, leaf_id):
     """
     This function reroots the tree at the specified leaf node.
     """
-    def find_path(node, path):
+    def find_path(node, leaf_id, path):
         """
         This helper function finds the path from the root to the leaf node.
         """
         if node is None:
             return False
         path.append(node)
-        if node.x == leaf_id:
-            return True
-        if find_path(node.l, path) or find_path(node.r, path):
+        if node.x == leaf_id or find_path(node.l, leaf_id, path) or find_path(node.r, leaf_id, path):
             return True
         path.pop()
         return False
 
-    path = []
-    find_path(tree, path)
+    # Reversing the parent-child relationship
+    def reverse_relationship(path):
+        for i in range(len(path) - 1, 0, -1):
+            current = path[i]
+            parent = path[i - 1]
 
-    for i in range(len(path) - 1, 0, -1):
-        current_node = path[i]
-        parent_node = path[i - 1]
-        # Swap children and parent
-        if parent_node.l == current_node:
-            parent_node.l = parent_node.r
-            parent_node.r = None
-        else:
-            parent_node.r = parent_node.l
-            parent_node.l = None
+            # Make the parent a child of the current node
+            if parent.l == current:
+                parent.l = None
+                current.r, parent.r = parent, current.r
+            elif parent.r == current:
+                parent.r = None
+                current.l, parent.l = parent, current.l
 
-        if current_node.l is None:
-            current_node.l = parent_node
-        else:
-            current_node.r = parent_node
-
-    # The last node in the path is the new root
-    if path:
-        path[-1].l = path[-1].r = None
+        # The last node in the path is the new root
         return path[-1]
+
+    path = []
+    find_path(tree, leaf_id, path)
+    return reverse_relationship(path) if path else None
