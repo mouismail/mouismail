174d02c5dc48ad54965b591dd493214c73626153
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:53:39 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -1,41 +1,62 @@
 class Tree:
-    def __init__(self, x, left=None, right=None):
-        self.x = x
-        self.l = left
-        self.r = right
+  """
+  This class represents a binary tree node.
+  """
+  def __init__(self, x, left=None, right=None):
+    self.x = x
+    self.l = left
+    self.r = right
 
-def solution(T, leaf_id):
-    # Convert tuple to Tree
-    def tuple_to_tree(t):
-        if not t:
-            return None
-        return Tree(t[0], tuple_to_tree(t[1]), tuple_to_tree(t[2]))
+def reroot(node, parent):
+    if node is None:
+        return parent
+    new_root = reroot(node.l, Tree(node.x, node.r, parent))
+    node.l = None
+    return new_root
 
-    # Find and reroot the tree
-    def find_and_reroot(current, parent):
-        if current is None:
-            return None
-        if current.x == leaf_id:
-            return reroot(current, parent)
-        # Recurse on left and right subtrees
-        left_result = find_and_reroot(current.l, Tree(current.x, None, parent))
-        if left_result is not None:
-            return left_result
-        right_result = find_and_reroot(current.r, Tree(current.x, parent, None))
-        return right_result
 
-    # Perform the rerooting
-    def reroot(node, parent):
-        if node is None:
-            return parent
-        # The order of the next two lines is changed to first process the right subtree
-        right_subtree = reroot(node.r, Tree(node.x, None, parent))
-        left_subtree = reroot(node.l, right_subtree)
-        node.l = None
-        node.r = None
-        return left_subtree
+def solution(tree, leaf_id):
+  """
+  This function reroots the tree at the specified leaf node.
+  """
+  # Helper function to convert tuple to Tree
+  def tuple_to_tree(node_tuple):
+    if node_tuple is None:
+      return None
+    return Tree(node_tuple[0], tuple_to_tree(node_tuple[1]), tuple_to_tree(node_tuple[2]))
 
-    # Convert the input tuple to a Tree
-    root = tuple_to_tree(T)
-    # Find the leaf and reroot the tree
-    return find_and_reroot(root, None)
+  # Helper function to find and reroot the tree
+  def find_and_reroot(current, parent, leaf_id):
+    if current is None:
+      return None
+    if current.x == leaf_id:
+      return reroot(current, parent)
+    # Move left subtree up, if the leaf is in the left subtree
+    left = find_and_reroot(current.l, Tree(current.x, parent, current.r), leaf_id)
+    if left:
+      return left
+    # Move right subtree up, if the leaf is in the right subtree
+    right = find_and_reroot(current.r, Tree(current.x, current.l, parent), leaf_id)
+    if right:
+      return right
+    return None
+
+  tree = tuple_to_tree(tree)
+  return find_and_reroot(tree, None, leaf_id)
+
+# Function to print the tree for verification purposes
+def print_tree(node, level=0):
+    if node is not None:
+        print_tree(node.r, level + 1)
+        print(' ' * 4 * level + '->', node.x)
+        print_tree(node.l, level + 1)
+
+# Convert the tuple to Tree instances
+tree_as_tuple = (3, (1, (2, None, None), (6, None, None)), (5, None, (4, None, None)))
+leaf_id_to_reroot = 2
+
+# Call the solution function with the tuple representation
+new_root = solution(tree_as_tuple, leaf_id_to_reroot)
+
+# Print the rerooted tree
+print_tree(new_root)
