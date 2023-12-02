b7507641959eb3ef39cf60f9b867ac7a9be2a05e
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:13:42 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -8,32 +8,28 @@ def solution(tree, leaf_id):
     def find_path(node, leaf_id, path):
         if node is None:
             return False
-        path.append(node)
-        if node.x == leaf_id or find_path(node.l, leaf_id, path) or find_path(node.r, leaf_id, path):
+        if node.x == leaf_id:
+            path.append(node)
             return True
-        path.pop()
+        if find_path(node.l, leaf_id, path) or find_path(node.r, leaf_id, path):
+            path.append(node)
+            return True
         return False
 
     # Reverse the parent-child relationship along the path
     def reverse_relationship(path):
-        for i in range(len(path) - 1, 0, -1):
-            current = path[i]
-            parent = path[i - 1]
+        for i in range(len(path) - 1):
+            child = path[i]
+            parent = path[i + 1]
 
-            # Check which side the current node is on and adjust accordingly
-            if parent.l == current:
-                parent.l = current.r
-                current.r = parent
-            elif parent.r == current:
-                parent.r = current.l
-                current.l = parent
+            # Swap the child with the parent's other subtree
+            if parent.l == child:
+                parent.l, child.r = child.r, parent
+            elif parent.r == child:
+                parent.r, child.l = child.l, parent
 
-        # The last node in the path is the new root
-        if path:
-            new_root = path[-1]
-            new_root.l = new_root.r = None
-            return new_root
-        return None
+        # The root of the new tree
+        return path[-1]
 
     path = []
     find_path(tree, leaf_id, path)
@@ -42,4 +38,3 @@ def solution(tree, leaf_id):
 # Example usage
 root = Tree(3, Tree(1, Tree(2), Tree(6)), Tree(5, None, Tree(4)))
 new_root = solution(root, 2)
-# new_root is now the new root of the tree with the tree rerooted at the node with x=2
