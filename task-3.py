8edcf1ba92aa3ba273954892a192ae900aec2009
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:05:31 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -1,35 +1,38 @@
+from extratypes import Tree  # Import the Tree class from extratypes module
+
 def solution(tree, leaf_id):
-    def find_path(node, path):
+    """
+    This function reroots the tree at the specified leaf node.
+    """
+    # Helper function to recursively find the path from root to the leaf
+    def find_path(node, leaf_id, path):
         if node is None:
             return False
         path.append(node)
-        if node.x == leaf_id:
-            return True
-        if find_path(node.l, path) or find_path(node.r, path):
+        if node.x == leaf_id or find_path(node.l, leaf_id, path) or find_path(node.r, leaf_id, path):
             return True
         path.pop()
         return False
 
+    # Reversing the parent-child relationship
+    def reverse_relationship(path):
+        new_root = path[-1]
+        for i in range(len(path) - 1, 0, -1):
+            current = path[i]
+            parent = path[i - 1]
+            if parent.l == current:
+                parent.l = None
+                current.r = parent
+            else:
+                parent.r = None
+                current.l = parent
+        return new_root
+
     path = []
-    find_path(tree, path)
+    find_path(tree, leaf_id, path)
+    return reverse_relationship(path) if path else None
 
-    for i in range(len(path) - 1, 0, -1):
-        current_node = path[i]
-        parent_node = path[i - 1]
-        # Swap children and parent
-        if parent_node.l == current_node:
-            parent_node.l = parent_node.r
-            parent_node.r = None
-        else:
-            parent_node.r = parent_node.l
-            parent_node.l = None
-
-        if current_node.l is None:
-            current_node.l = parent_node
-        else:
-            current_node.r = parent_node
-
-    # The last node in the path is the new root
-    if path:
-        path[-1].l = path[-1].r = None
-        return path[-1]
+# Example usage
+root = Tree(3, Tree(1, Tree(2), Tree(6)), Tree(5, None, Tree(4)))
+new_root = solution(root, 2)
+# new_root is now the new root of the tree with the tree rerooted at the node with x=2
