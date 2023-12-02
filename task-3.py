8aaee1b6762b3c711d7746867e6a948a5b984ee2
Mohammad Ismail <96207520+mouismail@users.noreply.github.com>
2023-12-02 21:04:45 +0100

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -1,15 +1,10 @@
 def solution(tree, leaf_id):
-    """
-    This function reroots the tree at the specified leaf node.
-    """
-    # Helper function to recursively find the path from root to the leaf
     def find_path(node, path):
         if node is None:
             return False
-        if node.x == leaf_id:
-            path.append(node)
-            return True
         path.append(node)
+        if node.x == leaf_id:
+            return True
         if find_path(node.l, path) or find_path(node.r, path):
             return True
         path.pop()
@@ -23,9 +18,11 @@ def solution(tree, leaf_id):
         parent_node = path[i - 1]
         # Swap children and parent
         if parent_node.l == current_node:
-            parent_node.l = None
-        else:
+            parent_node.l = parent_node.r
             parent_node.r = None
+        else:
+            parent_node.r = parent_node.l
+            parent_node.l = None
 
         if current_node.l is None:
             current_node.l = parent_node
